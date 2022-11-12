a67a378bc7d11905d797dabb15307fa43570d810
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-11-12 19:06:28 +0900

    parts

---
+++ src/components/parts/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface HeaderProps {}
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <HeaderStyle>
+      <h1>ViteReactDeno</h1>
+    </HeaderStyle>
+  );
+};
+
+const HeaderStyle = styled.header`
+position: sticky;
+top: 0;
+height: 70px;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: ${({ theme }) => theme.color.main};
+color: ${({ theme }) => theme.color.sub};
+`;
+export default Header;
